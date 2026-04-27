cbb6f2afd45e4933a7cdd039d96f56f51e8f676e
Rajmund <danyirajmund0929@gmail.com>
2026-04-27 08:00:16 +0200

    Create run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "=== Fake Update ==="
+./fake_update.sh
+
+echo "=== Fake Boot ==="
+./fake_boot.sh
+
+echo "=== Fake CPU ==="
+./fake_cpu.sh
+
+echo "=== Fake Backup ==="
+./fake_backup.sh
